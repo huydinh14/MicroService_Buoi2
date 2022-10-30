3f6f3d62edd22e49a57413508d5b925f4190dc73
Dinh Huy <huydinhse@gmail.com>
2022-10-30 21:08:04 +0700

    config server, Eureka, gateway, circui

---
+++ Eureka-server/src/test/java/iuh/dhktpm15a/kientrucpm/Eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.dhktpm15a.kientrucpm.Eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
